4dbf308f4fe243950fe6ccd7d29fac425c84ddd1
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-07-10 16:42:04 +0530

    Create Header.h

---
+++ Smallest Element SL/Header.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int Data;
+    struct Node *next;
+};
+
+typedef struct Node NODE;
+typedef struct Node *PNODE;
+typedef struct Node **PPNODE;
+
+void InsertFirst(PPNODE Head, int No);
+int Minimum(PNODE Head);
