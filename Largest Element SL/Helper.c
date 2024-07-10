1d099c62aa04c0c87289f13ab3ed2c9bd4078526
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-07-10 16:37:42 +0530

    Create Helper.c

---
+++ Largest Element SL/Helper.c
@@ -0,0 +1,58 @@
+#include "Header.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : InsertFirst
+// Description   : It takes address of First pointer of linked list and one integer and insert that integer in the linked list 
+// Input         : Address of First pointer , Integer 
+// Output        : None
+// Author        : Gaurav Mahadev Shinde
+// Date          : 4th July 2024
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void InsertFirst(PPNODE Head, int No)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+    newn->Data = No;
+    newn->next = NULL;
+
+    if (*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn->next = *Head;
+        *Head = newn;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Maximum
+// Description   : It takes value of First pointer of linked list and return maximum element of the LinkedList
+// Input         : Value of First pointer
+// Output        : Integer
+// Author        : Gaurav Mahadev Shinde
+// Date          : 4th July 2024
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int Maximum(PNODE Head)
+{
+    int iMax = Head->Data;
+
+    while (Head != NULL)
+    {
+        if (Head->Data > iMax)
+        {
+            iMax = Head->Data;
+        }
+        Head = Head->next;
+    }
+
+    return iMax;
+}
